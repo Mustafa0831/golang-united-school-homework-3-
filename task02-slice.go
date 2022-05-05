1de26092eaa0c0b5e6dbb450fbae7183568125df
Mustafa Ustaz <mr.mus0831@gmail.com>
2022-05-05 22:51:32 +0600

    3

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,21 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
+	// int64AsIntValues := make([]int, len(input))
+
+	// for i, val := range input {
+	// 	int64AsIntValues[i] = int(val)
+	// }
+	// sort.Ints(int64AsIntValues)
+	// for i, val := range int64AsIntValues {
+	// 	input[i] = int64(val)
+	// }
+	// for i, j := 0, len(input)-1; i < j; i, j = i+1, j-1 {
+	// 	input[i], input[j] = input[j], input[i]
+	// }
+	// copy(result, input)
+	// //Place your code here
+	// return
 	for i,j:=0,len(input)-1; i<j;i,j=i+1, j-1{
 		input[i],input[j]=input[j],input[i]
 	}
--- task03-map.go
+++ task03-map.go
@@ -2,11 +2,12 @@ package homework
 
 import "sort"
 
-func sortMapValues(input map[int]string) (result []string) {
+func sortMapValues(input map[int]string) []string {
+	result :=[]string{}
 	for _, v := range input {
 		result = append(result, v)
 	}
 	sort.Strings(result)
 	//Place your code here
-	return
+	return result
 }
